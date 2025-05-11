c66307409452e8b868ba94ad6f6430400e99fd61
albinr <albin@rybergs.net>
2025-05-11 13:09:32 +0200

    Inital start

---
+++ script.js
@@ -0,0 +1,5 @@
+// Form validation feedback
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thanks, Your message has been sent.")
+});
